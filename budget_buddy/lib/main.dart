e16a34160b2614b0600e2e279f2ac78849e29b11
Francesco Caglianone <francescocaglianone@gmail.com>
2024-09-12 16:47:57 +0200

    added color scheme

--- budget_buddy/lib/main.dart
+++ budget_buddy/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:budget_buddy/screens/expenses.dart';
 import 'package:flutter/material.dart';
 
+
+
+var myColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 60, 90, 78));
+
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,9 +17,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Budget Buddy',
-      home: Expenses()
+      theme: ThemeData().copyWith(
+        colorScheme: myColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: myColorScheme.onPrimaryContainer,
+          foregroundColor: myColorScheme.primaryContainer,
+        )
+      ),
+      home: const Expenses()
     );
   }
 }
